614a7d46b35b88ea3121ca0c6a7e742d611dc1f7
Ali Alkhateeb <drastic67@gmail.com>
2020-10-09 09:42:00 +0000

    Created the local database using models.py and added database info in app.py, added new files to gitignore

---
+++ models.py
@@ -0,0 +1,14 @@
+# models.py
+import flask_sqlalchemy
+from app import db
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(120))
+    
+    def __init__(self, a):
+        self.message = a
+        
+    def __repr__(self):
+        return '<message: %s>' % self.message
